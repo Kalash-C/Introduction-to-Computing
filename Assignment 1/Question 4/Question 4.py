7388bc6a8d52e9de17cf682f09304c880db4acdd
Kalash-C <97087794+Kalash-C@users.noreply.github.com>
2022-01-06 18:08:52 +0530

    Delete Assignment 1 directory

--- Assignment 1/Question 4/Question 4.py
+++
@@ -1,9 +0,0 @@
-# Displaying marks in a sorted way in a list
-marks1=int(input('Enter marks of student 1: '))
-marks2=int(input('Enter marks of student 2: '))
-marks3=int(input('Enter marks of student 3: '))
-marks4=int(input('Enter marks of student 4: '))
-marks5=int(input('Enter marks of student 5: '))
-list=[marks1, marks2, marks3, marks4, marks5]
-list.sort(reverse=True)
-print(list)
--- Assignment 1/Question 5/Question 5 Output.py
+++
@@ -1,8 +0,0 @@
-Python 3.8.10 (default, Nov 26 2021, 20:14:08) 
-[GCC 9.3.0] on linux
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-======= RESTART: /home/kc/Downloads/Assignment 1/Question 5/Question 5.py ======
-Orignal list is:  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-List after removing an element:  ['Red', 'Green', 'White', 'Pink', 'Yellow']
->>> 

ef3286a2be88f3df1a51ee9f0dce104ea2502048
Kalash-C <97087794+Kalash-C@users.noreply.github.com>
2022-01-06 18:11:49 +0530

    Update Assignment 1 Output.py

--- Assignment 1/Assignment 1 Output.py
+++ Assignment 1/Assignment 1 Output.py
@@ -3,26 +3,35 @@ Python 3.8.10 (default, Nov 26 2021, 20:14:08)
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
 ======= RESTART: /home/kc/Downloads/Assignment 1/Question 1/Question 1.py ======
+#Question 1
 Enter Number 1: 200
 Enter Number 2: 56
 Enter Number 3: 87
 Average of 3 Number:  114.33333333333333
+
+#Question 2
 Gross Income is: 32110
 Number of Dependents: 3
 Taxable Income is:  13110.0
 Tax is:  2622.0
+  
+#Question 3
 Enter your SID: 21105115
 Enter your Name: Kalash
 Enter your Gender(Male: M, Female: F, Unknown: U): M
 Enter your Course Name: ECE
 Enter your CGPA: 8.0
 [21105115, 'Kalash', 'M', 'ECE', 8.0]
+
+#Question 4
 Enter marks of student 1: 85
 Enter marks of student 2: 76
 Enter marks of student 3: 92
 Enter marks of student 4: 54
 Enter marks of student 5: 45
 [92, 85, 76, 54, 45]
+
+#Question 5
 Orignal list is:  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
 List after removing an element:  ['Red', 'Green', 'White', 'Pink', 'Yellow']
 List after adding purple:  ['Red', 'Green', 'White', 'Purple', 'Yellow']

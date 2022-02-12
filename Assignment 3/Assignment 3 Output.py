883070f4e73fdf05ebaa604f515431e4cad28843
Kalash-C <97087794+Kalash-C@users.noreply.github.com>
2022-02-12 14:10:22 +0530

    Update Assignment 3 Output.py

--- Assignment 3/Assignment 3 Output.py
+++ Assignment 3/Assignment 3 Output.py
@@ -5,17 +5,20 @@ Type "help", "copyright", "credits" or "license()" for more information.
 Question 1
 Enter a string: hello world
 {'hello': 1, 'world': 1}
+
 Question 2
 Enter Day: 29
 Enter Month: 2
 Enter Year (1800-2025): 1900
 Next date is:  1 / 3 / 1900
+  
 Question 3
 How many numbers do you want to add: 3
 Enter a number: 2
 Enter a number: 5
 Enter a number: 9
 [(2, 4), (5, 25), (9, 81)]
+
 Question 4
 Enter the grade point: 7
 Your grade is "B" and Good perofrmance

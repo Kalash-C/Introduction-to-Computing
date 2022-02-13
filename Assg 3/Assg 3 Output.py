777e7a035d935966df1621874b80ea0042ffae33
Kalash-C <97087794+Kalash-C@users.noreply.github.com>
2022-02-13 11:34:34 +0530

    Add files via upload

---
+++ Assg 3/Assg 3 Output.py
@@ -0,0 +1,83 @@
+Python 3.9.10 (tags/v3.9.10:f2f3f53, Jan 17 2022, 15:14:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:\Users\kalas\Desktop\Assignment 3\Assignment 3.py =========
+Question 1
+
+Enter a string: hello world
+{'hello': 1, 'world': 1}
+
+
+Question 2
+
+Enter Day: 29
+Enter Month: 2
+Enter Year (1800-2025): 1900
+Next date is:  1 / 3 / 1900
+
+
+Question 3
+
+How many numbers do you want to add: 3
+Enter a number: 3
+Enter a number: 7
+Enter a number: 9
+[(3, 9), (7, 49), (9, 81)]
+
+
+Question 4
+
+Enter the grade point: 7
+Your grade is "B" and Good perofrmance
+
+
+Question 5
+
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+
+Question 6
+
+Enter sid: 21105115
+Enter name: Kalash
+Do you want to enter more data (Use "Y" or "N"): Y
+Enter sid: 102
+Enter name: abc
+Do you want to enter more data (Use "Y" or "N"): Y
+Enter sid: 101
+Enter name: xyz
+Do you want to enter more data (Use "Y" or "N"): N
+Data has been added
+The data entered is  {21105115: 'Kalash', 102: 'abc', 101: 'xyz'}
+Dictionary sorted by name  [(102, 'abc'), (21105115, 'Kalash'), (101, 'xyz')]
+Dictionary sorted by sid  [(101, 'xyz'), (102, 'abc'), (21105115, 'Kalash')]
+Enter the SID: 101
+The name of the student is  xyz
+
+
+Question 7
+
+Enter the number of terms: 7
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+The average of the fibonacci series of given 7 terms is 2.857142857142857
+
+
+
+Elements that are in st1 and set2 but not both are {1, 3, 5, 6, 8}
+Elements that are only in one of set1, set2 and set3 are {17, 3, 6, 8, 9, 13}
+Elements that are exactly in 2 sets are {1, 2, 4, 5}
+Elements from 1 to 10 that are not in set1 are {8, 9, 6, 7}
+Elements from 1 to 10 that are not in any of the given sets are {7}
+>>> 

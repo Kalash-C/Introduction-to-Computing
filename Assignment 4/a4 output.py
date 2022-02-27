e89eb37edf534deb095a310c72b420494fcb1e2a
Kalash-C <97087794+Kalash-C@users.noreply.github.com>
2022-02-27 17:10:38 +0530

    Update a4 output.py

--- Assignment 4/a4 output.py
+++ Assignment 4/a4 output.py
@@ -2,6 +2,7 @@ Python 3.9.10 (tags/v3.9.10:f2f3f53, Jan 17 2022, 15:14:21) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
 ============ RESTART: C:\Users\kalas\Desktop\Assignment 4\a4 code.py ===========
+ #Question1
 Move disk [1] from rod [ A ] to rod { B }
 Move disk [2] from rod [ A ] to rod { C }
 Move disk [1] from rod [ B ] to rod { C }
@@ -17,6 +18,9 @@ Move disk [3] from rod [ B ] to rod { C }
 Move disk [1] from rod [ A ] to rod { B }
 Move disk [2] from rod [ A ] to rod { C }
 Move disk [1] from rod [ B ] to rod { C }
+
+
+#Question 2
 Enter number of rows: 5
       1
      1 1
@@ -28,6 +32,9 @@ Enter number of rows: 5
 [1, 2, 1]
 [1, 3, 3, 1]
 [1, 4, 6, 4, 1]
+
+
+#Question 3
 Enter 1st number: 23
 Enter 2nd number: 32
 (0, 23)
@@ -37,10 +44,19 @@ There is a zero
 {0, 4, 5, 6, 23}
 frozenset({0, 4, 5, 6, 23})
 max value is 23 hash val of max val is 23
+
+
+#Question4
 Name of student is abc and roll number is 11
 Object destroyed
+
+
+#Question 5
 {'Name': {'Mehak', 'Viren', 'Ashok'}, 'Salary': {40000, 50000, 60000}}
 {'name': 'Mehak', 'salary': 70000}
+
+
+#Question6
 Enter a word : tea
 Enter a new meaningful word : eat
 Friendship is real
